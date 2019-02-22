9abfc85a95e4a995a9122559f121fd9bb05f6fd1
aoliveira <to.oliveira@gmail.com>
2019-02-22 23:04:14 +0000

    ADS126x: support full range of SPS ADS126x: support all range of digital filters ADS126x: optimized conversion delays. reduced polling ADS126x: refactored to hide private methods

--- src/drivers/ads126x.cpp
+++ src/drivers/ads126x.cpp
@@ -15,6 +15,7 @@
 
 #include <common/adc.hpp>
 #include <common/peripheral_type.hpp>
+#include <common/result.hpp>
 
 #include <ads126x.hpp>
 #include <ads126x_config.hpp>
@@ -37,7 +38,7 @@ static int spi_raw_xfer( int fd , void const *wrBuf , void *rdBuf , size_t len )
     xfer[0].bits_per_word = 8;
     xfer[0].speed_hz=25000000;
     
-    usleep(100);
+    //usleep(100);???
     status  = ioctl(fd,SPI_IOC_MESSAGE(1),xfer);
     if ( status<0 ) {
         perror("SPI_IOC_MESSAGE");
@@ -45,7 +46,6 @@ static int spi_raw_xfer( int fd , void const *wrBuf , void *rdBuf , size_t len )
     }
     return 0;
 }
-
 
 
 static void spi_dumpstat(const char *name, int fd)
@@ -77,6 +77,103 @@ static void spi_dumpstat(const char *name, int fd)
 
 
 
+namespace Tables {
+namespace Adc1 {
+
+/**
+ * @brief Get the Index of Samples per Second for ADC1 (32b core)
+ * 
+ * @param sps Samples Per Second string, as given in option "sps"
+ * @return int The Samples per Second bits. This returned value is valid
+ *              for lookups in constants tables, as well as for direct
+ *              usage in the register config for DAC1_SPS
+ */
+static int getIdOfSps( std::string const &sps )
+{
+    if ( sps == "2.5" )     return 0;
+    if ( sps == "5" )       return 1;
+    if ( sps == "10" )      return 2;
+    if ( sps == "16.6" )    return 3;
+    if ( sps == "20" )      return 4;
+    if ( sps == "50" )      return 5;
+    if ( sps == "60" )      return 6;
+    if ( sps == "100" )     return 7;
+    if ( sps == "400" )     return 8;
+    if ( sps == "1200" )    return 9;
+    if ( sps == "2400" )    return 10;
+    if ( sps == "4800" )    return 11;
+    if ( sps == "7200" )    return 12;
+    if ( sps == "14400" )   return 13;
+    if ( sps == "19200" )   return 14;
+    if ( sps == "38400" )   return 15;
+    return -1;
+}
+
+static int getIdOfFilter( std::string const filter )
+ {
+    if ( filter == "sinc1" ) return 0;
+    if ( filter == "sinc2" ) return 1;
+    if ( filter == "sinc3" ) return 2;
+    if ( filter == "sinc4" ) return 3;
+    if ( filter == "sinc5" ) return 4;
+    if ( filter == "sinc1" ) return 5;
+    return -1;
+}
+
+
+
+
+static int CONV_DURATION[16][6] = {
+/* SPS          SINC1     SINC2     SINC3     SINC4   SINC5      FIR */
+/*     2.5*/{  400400 ,  800400 , 1200000 , 1600000 ,    -1 , 402200 } ,
+/*     5 */ {  200400 ,  400400 ,  600400 ,  800400 ,    -1 , 202200 } , 
+/*    10 */ {  100400 ,  200400 ,  300400 ,  400400 ,    -1 , 102200 } ,
+/*    16.6*/{   60350 ,  120400 ,  180400 ,  240400 ,    -1 ,     -1 } ,
+/*    20 */ {   50350 ,  100400 ,  150400 ,  200400 ,    -1 ,  52220 } , 
+/*    50 */ {   20350 ,   40420 ,   60420 ,   80420 ,    -1 ,     -1 },
+/*    60 */ {   17020 ,   33760 ,   50420 ,   67090 ,    -1 ,     -1 } ,
+/*   100 */ {   10350 ,   20420 ,   30420 ,   40420 ,    -1 ,     -1 } ,
+/*   400 */ {    2855 ,    5424 ,    7924 ,   10420 ,    -1 ,     -1 } ,
+/*  1200 */ {    1188 ,    2091 ,    2924 ,    3758 ,    -1 ,     -1 } ,
+/*  2400 */ {     771 ,    1258 ,    1674 ,    2091 ,    -1 ,     -1 } ,
+/*  4800 */ {     563 ,     841 ,    1049 ,    1258 ,    -1 ,     -1 } ,
+/*  7200 */ {     494 ,     702 ,     841 ,     980 ,    -1 ,     -1 } ,
+/* 14400 */ {      -1 ,      -1 ,      -1 ,      -1 ,   424 ,     -1 } ,
+/* 19200 */ {      -1 ,      -1 ,      -1 ,      -1 ,   337 ,     -1 } ,
+/* 38400 */ {      -1 ,      -1 ,      -1 ,      -1 ,   207 ,     -1 }
+};
+
+
+/** Map SPS_ID into register value */
+static int SPS_REG_VAL[16] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+
+/** Map FILTER_ID into register value
+ * @note SINC5 is special case for SPS>=14400, meaning the value will
+ *          be actually ifgnored and 2nd stage SINC bypassed.
+ */
+static int FILTER_REG_VAL[6] =
+{
+    0 , /* SINC1 */
+    1 , /* SINC2 */
+    2 , /* SINC3 */
+    3 , /* SINC4 */
+    4 , /* SINC5 - register value is ignored, as the second SINC stage */
+    4 , /* FIR   */
+};
+
+}; /* namespace Tables::Adc1 */
+}; /* namespace Tables */
+
+
+
+
+typedef enum {
+    ADS1262_FILTER_SINC1 = 0 ,
+    ADS1262_FILTER_SINC2 = 1 ,
+    ADS1262_FILTER_SINC3 = 2 ,    
+    ADS1262_FILTER_SINC4 = 3 ,
+    ADS1262_FILTER_FIR   = 4 ,
+} ads1262_filter_t;
 
 
 class Ads126x::Ads126xImpl
@@ -89,9 +186,19 @@ private:
     double      myVref;
     std::vector<double> myInpGain;
     std::list<string> myIdacMagnitudes;
+    std::list<string> mySpsValues;
+    std::list<string> myFilterValues;
     
     std::string myModel;
     std::string myRevision;
+
+    /* options */
+    std::string myOptSps; /*< Samples Per Second */
+    std::string myOptFilter; /*< Filter */
+    int myCurSpsId;
+    int myCurFilterId;
+    int myCurConvTime;
+    int myCurConvPoll;
     
     Ads126xInput getChInput( unsigned int chId )
     {
@@ -123,106 +230,21 @@ public:
         assert( cfg.inpGain.size()==10 );
         myInpGain   = cfg.inpGain;
         myIdacMagnitudes    = {"50u" ,"100u","250u","500u","750u","1000u","1500u","2000u","3000u" };
+        mySpsValues         = { "2.5","5","10","16.6","20","50","60","100","400","1200",
+                                "2400","4800","7200","14400","19200","38400" };
+        /* SINC5 filter not offered as option as it becomes active
+         * for SPS>=14400, enforced by the chip itself...
+         */
+        myFilterValues      = {"sinc1","sinc2","sinc3","sinc4","fir"};
     }
     
     ~Ads126xImpl()
     {
         close( myFd );
     }
+
+private:
     
-    void init()
-    {   
-        uint8_t value;
-        
-        myFd    = open(mySpiDev.c_str(), O_RDWR);
-        if ( myFd < 0) {
-            perror("opening spidev:");
-            return;
-        }
-        
-        uint32_t mode   = 1;
-        int res         = ioctl(myFd,SPI_IOC_WR_MODE32,&mode);
-        if ( res!=0 ) {
-            perror("SPI_IOC_WR_MODE32:");
-            close(myFd);
-            return;
-        }
-
-
-        spi_dumpstat(mySpiDev.c_str(),myFd);
-
-         /*spi_dumpstat(mySpiDev,myFd);*/
-         doCommand(ADS126X_CMD_RESET);
-         usleep( 200000 );
-        
-        value    = ADS126X_INTERFACE_STATUS_INCLUDED
-                                    | ADS126X_INTERFACE_CKSUM;
-        writeSingleReg(ADS126X_REG_INTERFACE,value);
-
-        uint8_t u8=0;
-        readRegisters(ADS126X_REG_INTERFACE,1,&u8);
-        if ( u8!=value ) {
-            cerr << "Mismatching values in INTERFACE register: written=" << value << " readback=" << u8 << "\n";
-        }
-        
-        
-        readRegisters(ADS126X_REG_ID,1,&value);
-        if ( (value&0xe0)==0x00 ) {
-            myModel = "ADS1262";
-        }
-        else if ( (value&0xe0)==0x20 ) {
-            myModel = "ADS1263";
-        }
-        else {
-            fprintf( stderr , "Detected unknown DAC: DEV_ID=0x%x\n" , value>>5 );
-            myModel = "DEV_ID_UNKNOWN(" + std::to_string(value>>5) + ")";
-        }
-        
-        myRevision = std::to_string(value&0x1f);
-        std::cerr << "Detected ADC: " << myModel << " rev. " << myRevision << "\n";
-
-        value   = ADS126X_POWER_VBIAS_OFF | ADS126X_POWER_INTREF_ON;
-        writeSingleReg(ADS126X_REG_POWER,value);
-        readRegisters(ADS126X_REG_POWER,1,&u8);
-        if ( u8!=value ) {
-            cerr << "Mismatching values in POWER register: written=" << value << " readback=" << u8 << "\n";
-        }
-
-        /* several configs mostly for testing..... */
-        
-        writeSingleReg(ADS126X_REG_MODE0,0x1b);
-        writeSingleReg(ADS126X_REG_MODE1,0x00);
-        writeSingleReg(ADS126X_REG_MODE2,0x04);
-        writeSingleReg(ADS126X_REG_IDACMUX,0xbb);
-        writeSingleReg(ADS126X_REG_IDACMAG,0x00);
-        writeSingleReg(ADS126X_REG_REFMUX,0x00);
-        writeSingleReg(ADS126X_REG_TDACP,0x00);
-        writeSingleReg(ADS126X_REG_TDACN,0x00);
-        writeSingleReg(ADS126X_REG_GPIOCON,0x00);
-        writeSingleReg(ADS126X_REG_FSCAL0,0x00);
-        writeSingleReg(ADS126X_REG_FSCAL1,0x00);
-        writeSingleReg(ADS126X_REG_FSCAL2,0x40);
-        
-        uint8_t rd[3];
-        rd[0] = rd[1] = rd[2] = 0;
-        
-        readRegisters(ADS126X_REG_FSCAL0,3,rd);
-        printf( "fscal0=0x%02x\nfscal1=0x%02x\nfscal2=0x%02x\n",rd[0],rd[1],rd[2] );        
-    }
-
-    void calibrate()
-    {
-        //printf( "Performing self-calibration...\n");
-        setInput(ADS126x_AINCOM,ADS126x_AINCOM);
-        doCommand(ADS126X_CMD_SYOCAL1);
-        sleep(1);
-        setInput(ADS126x_FLOAT,ADS126x_FLOAT);
-        doCommand(ADS126X_CMD_SFOCAL1);
-        sleep(1);
-    //    ads126x_sygcal1(adc);
-    //    sleep(2);
-    }
-
     void dumpRegisters()
     {
         uint8_t regDump[0x1b];
@@ -288,7 +310,7 @@ public:
                     cerr << "Number of iterations exceeded. Giving up.\n";
                     break;
                 }
-                usleep(100);
+                usleep(myCurConvPoll);
             }
         } while ( !adcDone );
         if ( iteration>10 ) {
@@ -360,6 +382,72 @@ public:
         }
         return 0;
     }
+
+    /**
+     * @brief Set the ADC1 FILTER in reg. MODE1
+     * Sets bits 7:5 into value provided in parameter.
+     * 
+     * @param filterRegVal value for FILTER setting, before bitshifts.
+     * @return Result 
+     */
+    Result setAdc1Filter( int filterRegVal )
+    {
+        uint8_t regVal;
+        readRegisters( ADS126X_REG_MODE1 , 1 , &regVal );
+        regVal &= 0x1f;
+        regVal |= (filterRegVal<<5);
+        writeSingleReg(ADS126X_REG_MODE1,regVal);
+        return Result::OK;
+    }
+
+    /**
+     * @brief Set ADC1 DR in reg. MODE2
+     * Sets bits 3:0 of register to value provided in parameter.
+     * 
+     * @param spsRegVal value for DR setting, before shifts
+     * @return Result 
+     */
+    Result setAdc1Sps( int spsRegVal )
+    {
+        uint8_t regVal;
+        readRegisters(ADS126X_REG_MODE2,1,&regVal);
+        regVal &= 0xf0;
+        regVal |= spsRegVal;
+        writeSingleReg(ADS126X_REG_MODE2,regVal);
+        return Result::OK;
+    }
+
+    /**
+     * @brief Recalculate conversion delays.
+     * Call this method once myCurFilterId and myCurSpsId are set
+     * to recalculate the optimal delays to be used when polling 
+     * the ADC for conversion completion.
+     */
+    void recalculateDelays()
+    {
+        myCurConvTime = Tables::Adc1::CONV_DURATION[myCurSpsId][myCurFilterId];
+        myCurConvPoll = myCurConvTime*5/100;
+        if ( myCurConvPoll<50 ) {
+            myCurConvPoll   = 50;
+        }
+    }
+
+    
+public:
+
+    void calibrate()
+    {
+        //printf( "Performing self-calibration...\n");
+        setInput(ADS126x_AINCOM,ADS126x_AINCOM);
+        doCommand(ADS126X_CMD_SYOCAL1);
+        sleep(1);
+        setInput(ADS126x_FLOAT,ADS126x_FLOAT);
+        doCommand(ADS126X_CMD_SFOCAL1);
+        sleep(1);
+    //    ads126x_sygcal1(adc);
+    //    sleep(2);
+    }
+
     
     int setInput( Ads126xInput input )
     {
@@ -403,14 +491,14 @@ public:
     {
         setInput( getChInput(chId) );
         doCommand(ADS126X_CMD_START1);
-        usleep(50000); /* hardcoded for ~20SPS */
+        usleep(myCurConvTime);
     }
 
     void startConversion( unsigned int chp, unsigned int chn )
     {
         setInput( getChInput(chp) , getChInput(chn) );
         doCommand(ADS126X_CMD_START1);
-        usleep(50000); /* hardcoded for ~20SPS */
+        usleep(myCurConvTime);
     }
 
 
@@ -553,6 +641,156 @@ public:
         fprintf(stdout,"READ(again): mux=0x%02x mag=0x%02x\n",idacCtl[0],idacCtl[1]);
         return 0;
 
+    }
+
+    Result setSps( std::string const &value )
+    {
+        int spsId = Tables::Adc1::getIdOfSps(value);
+        if ( spsId==-1 ) return Result::E_BAD_ARGS;
+
+        int delay = Tables::Adc1::CONV_DURATION[spsId][myCurFilterId];
+        if ( delay == -1 ) {
+            /* SPS and FILTER don't combine.
+             * try setting filter to "SINC1" as it delivers nominal SPS
+             * up to 14400sps...
+             */
+            int filterId    = Tables::Adc1::getIdOfFilter("sinc1");
+            delay           = Tables::Adc1::CONV_DURATION[spsId][filterId];
+            if ( delay == -1 ) {
+                /* only way of landing here is to have SPS > 14400
+                 * in this case the chip will enforce SINC5 do to
+                 * the bypass of the 2nd stage SINC.
+                 */
+                myOptFilter   = "sinc5";
+                myCurFilterId = Tables::Adc1::getIdOfFilter("sinc5");
+                /* no need to configure a filter, as it is a setting
+                 * enforced by the chip.
+                 */
+            }
+            else {
+                myOptFilter     = "sinc1";
+                myCurFilterId   = Tables::Adc1::getIdOfFilter("sinc1");
+                setAdc1Filter( Tables::Adc1::FILTER_REG_VAL[myCurFilterId] );
+            }
+        }
+        myOptSps    = value;
+        myCurSpsId  = spsId;
+        setAdc1Sps( Tables::Adc1::SPS_REG_VAL[myCurSpsId] );
+        recalculateDelays();
+
+        return Result::OK;
+    }
+
+    Result setFilter( std::string const &value )
+    {
+        int filterId = Tables::Adc1::getIdOfFilter(value);
+        if ( filterId == -1 ) return Result::E_BAD_ARGS;
+
+        int delay   = Tables::Adc1::CONV_DURATION[myCurSpsId][filterId];
+        if ( delay == -1 ) return Result::E_NOT_SUPPORTED;
+
+        setAdc1Filter( Tables::Adc1::FILTER_REG_VAL[filterId] );
+        myCurFilterId   = filterId;
+        myOptFilter     = value;
+        recalculateDelays();
+        return Result::OK;
+    }
+
+    Result setOption( std::string const &name , std::string const &value )
+    {
+        if ( name=="sps" ) return setSps(value);
+        if ( name=="filter" ) return setFilter(value);
+        return Result::E_NOT_SUPPORTED;
+    }
+
+    void init()
+    {   
+        uint8_t value;
+        
+        myFd    = open(mySpiDev.c_str(), O_RDWR);
+        if ( myFd < 0) {
+            perror("opening spidev:");
+            return;
+        }
+        
+        uint32_t mode   = 1;
+        int res         = ioctl(myFd,SPI_IOC_WR_MODE32,&mode);
+        if ( res!=0 ) {
+            perror("SPI_IOC_WR_MODE32:");
+            close(myFd);
+            return;
+        }
+
+
+        spi_dumpstat(mySpiDev.c_str(),myFd);
+
+         /*spi_dumpstat(mySpiDev,myFd);*/
+         doCommand(ADS126X_CMD_RESET);
+         usleep( 200000 );
+        
+        value    = ADS126X_INTERFACE_STATUS_INCLUDED
+                                    | ADS126X_INTERFACE_CKSUM;
+        writeSingleReg(ADS126X_REG_INTERFACE,value);
+
+        uint8_t u8=0;
+        readRegisters(ADS126X_REG_INTERFACE,1,&u8);
+        if ( u8!=value ) {
+            cerr << "Mismatching values in INTERFACE register: written=" << value << " readback=" << u8 << "\n";
+        }
+        
+        
+        readRegisters(ADS126X_REG_ID,1,&value);
+        if ( (value&0xe0)==0x00 ) {
+            myModel = "ADS1262";
+        }
+        else if ( (value&0xe0)==0x20 ) {
+            myModel = "ADS1263";
+        }
+        else {
+            fprintf( stderr , "Detected unknown DAC: DEV_ID=0x%x\n" , value>>5 );
+            myModel = "DEV_ID_UNKNOWN(" + std::to_string(value>>5) + ")";
+        }
+        
+        myRevision = std::to_string(value&0x1f);
+        std::cerr << "Detected ADC: " << myModel << " rev. " << myRevision << "\n";
+
+        value   = ADS126X_POWER_VBIAS_OFF | ADS126X_POWER_INTREF_ON;
+        writeSingleReg(ADS126X_REG_POWER,value);
+        readRegisters(ADS126X_REG_POWER,1,&u8);
+        if ( u8!=value ) {
+            cerr << "Mismatching values in POWER register: written=" << value << " readback=" << u8 << "\n";
+        }
+
+        /* several configs mostly for testing..... */
+        
+        writeSingleReg(ADS126X_REG_MODE0,0x1b);
+
+        /* MODE1 set to SINC1 */
+        myCurFilterId   = Tables::Adc1::getIdOfFilter("sinc1");
+        myOptFilter     = "sinc1";
+        writeSingleReg(ADS126X_REG_MODE1,0x00);
+
+        /* MODE2 set to 2400 sps */
+        myCurSpsId      = Tables::Adc1::getIdOfSps("2400");
+        myOptSps        = "2400";
+        writeSingleReg(ADS126X_REG_MODE2,0x0a);
+        writeSingleReg(ADS126X_REG_IDACMUX,0xbb);
+        writeSingleReg(ADS126X_REG_IDACMAG,0x00);
+        writeSingleReg(ADS126X_REG_REFMUX,0x00);
+        writeSingleReg(ADS126X_REG_TDACP,0x00);
+        writeSingleReg(ADS126X_REG_TDACN,0x00);
+        writeSingleReg(ADS126X_REG_GPIOCON,0x00);
+        writeSingleReg(ADS126X_REG_FSCAL0,0x00);
+        writeSingleReg(ADS126X_REG_FSCAL1,0x00);
+        writeSingleReg(ADS126X_REG_FSCAL2,0x40);
+
+        recalculateDelays();
+        
+        uint8_t rd[3];
+        rd[0] = rd[1] = rd[2] = 0;
+        
+        readRegisters(ADS126X_REG_FSCAL0,3,rd);
+        printf( "fscal0=0x%02x\nfscal1=0x%02x\nfscal2=0x%02x\n",rd[0],rd[1],rd[2] );        
     }
    
 
